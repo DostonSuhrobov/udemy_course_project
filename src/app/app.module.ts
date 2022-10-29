d947b8e5f0c2156c5049d367f3d6266fd10252c7
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-10-29 10:31:04 -0400

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './receipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './receipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    ReceipesComponent,
+
   ],
   imports: [
     BrowserModule
